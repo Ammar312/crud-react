1493dafd788153fb6836c66cf60be1c23376da8d
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-06 12:25:35 +0500

    host on server

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div className=""></div>;
+  return (
+    <div className=" bg-red-300">
+      hello world
+      <h1>hello server with react</h1>
+    </div>
+  );
 };
 
 export default App;
