b2e00ef9d01a27ffadab2d1175ae1204c50a8b1c
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-06 17:25:10 +0500

    form created

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import CreatePost from "./components/CreatePost";
 
 const App = () => {
   return (
-    <div className=" bg-red-300">
-      hello world
-      <h1>hello server with react</h1>
+    <div className="">
+      <CreatePost />
     </div>
   );
 };
--- web/src/components/CreatePost.jsx
+++ web/src/components/CreatePost.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const CreatePost = () => {
-  return <div></div>;
+  const titleInput = useRef();
+  const bodyInput = useRef();
+  const submitPost = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <div className=" border-2 border-purple-500 max-w-2xl">
+        <form onSubmit={submitPost} className=" flex flex-col gap-2 p-4">
+          <input
+            className="border-2 p-2 text-lg"
+            type="text"
+            required
+            placeholder="Title of the post"
+            minLength={3}
+            maxLength={30}
+            ref={titleInput}
+          />
+          <textarea
+            type="text"
+            required
+            placeholder="What's in your mind!"
+            minLength={3}
+            ref={bodyInput}
+            rows="3"
+            className="border-2 p-2 text-lg outline-none "
+          ></textarea>
+          <button type="submit" className="border-2 w-44 p-3 rounded-xl">
+            Publish
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
